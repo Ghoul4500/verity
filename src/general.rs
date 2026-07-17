4158fb20ad44c7ecb39ec74ad6008933339755e4
Ahmed Yaseen <yaseen@ghoul.dev>
2026-07-17 23:18:47 +0500

    feat(general): improve ping command

--- src/general.rs
+++ src/general.rs
@@ -1,16 +1,38 @@
 use crate::{Context, Error};
 
+use chrono::Utc;
+use indoc::formatdoc;
+use poise::serenity_prelude as serenity;
+use serenity::builder;
+
 /// Ping the bot to check latency
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     let ping = ctx.ping().await;
+    let message_time = *ctx.created_at();
 
-    if ping.is_zero() {
-        ctx.say("Ping not ready yet. Try again in a few seconds")
-            .await?;
-    } else {
-        ctx.say(format!("Pong!\nThat took **{}ms**", ping.as_millis()))
-            .await?;
-    }
+    let embed = builder::CreateEmbed::new()
+        .title("Pong!")
+        .color(serenity::Color::BLUE)
+        .description(formatdoc!(
+            "
+            ```rs
+            Websocket Latency:      {}
+            Message Latency:        {}ms
+            ```
+            ",
+            match ping.as_millis() {
+                0 => "N/A".to_string(),
+                _ => format!("{}ms", ping.as_millis()),
+            },
+            (Utc::now() - message_time).num_milliseconds()
+        ))
+        .footer(
+            builder::CreateEmbedFooter::new(format!("Requested by {}", ctx.author().name.clone()))
+                .icon_url(ctx.author().face()),
+        );
+
+    ctx.send(poise::CreateReply::default().embed(embed)).await?;
+
     Ok(())
 }
