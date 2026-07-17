c5eca48c51afe6aa6516d7c56fba4827ed861d09
Ahmed Yaseen <yaseen@ghoul.dev>
2026-07-17 18:35:43 +0500

    fix(ping): fix 0ms initial ping

--- src/general.rs
+++ src/general.rs
@@ -3,10 +3,14 @@ use crate::{Context, Error};
 /// Ping the bot to check latency
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say(format!(
-        "Pong!\nThat took **{}ms**",
-        ctx.ping().await.as_millis()
-    ))
-    .await?;
+    let ping = ctx.ping().await;
+
+    if ping.is_zero() {
+        ctx.say("Ping not ready yet. Try again in a few seconds")
+            .await?;
+    } else {
+        ctx.say(format!("Pong!\nThat took **{}ms**", ping.as_millis()))
+            .await?;
+    }
     Ok(())
 }
